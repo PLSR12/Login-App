fcaad011e443762171ebfe89cdcd0d7d98df7762
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-10-27 21:17:46 -0300

    [+] created login screen

--- App.tsx
+++ App.tsx
@@ -1,13 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native'
+import { TextField } from './components/Input'
+import { useForm } from 'react-hook-form'
+import { useEffect } from 'react'
 
 export default function App() {
+  const { register, setValue, handleSubmit } = useForm()
+
+  const onSubmit = (data: any) => {
+    console.log(data)
+  }
+
+  useEffect(() => {
+    register('email')
+  }, [register])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Faça Login</Text>
+      <TextField
+        label="Label"
+        onChangeText={(text: any) => setValue('email', text)}
+      />
+      <TextInput onChangeText={(text: any) => setValue('email', text)} />
+      <Button title="enviar" onPress={handleSubmit(onSubmit)} />
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +43,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
