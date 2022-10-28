812ce164817a9a2a06603d446c51a7a1c0e9c34d
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-10-27 21:17:32 -0300

    [+] created component input

---
+++ components/Input/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {},
+  label: {
+    fontSize: 20,
+    color: '#000',
+    fontWeight: 'bold',
+  },
+  input: {
+    width: 150,
+    borderWidth: 2,
+    borderRadius: 10,
+  },
+})
