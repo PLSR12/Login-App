33f0fc8a0e5bccbd871ec379aad1daf0989f7d0f
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-11-02 13:40:42 -0300

    [*] updated App

--- App.tsx
+++ App.tsx
@@ -1,49 +1,10 @@
 import { StatusBar } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-
-import {
-  Inter_400Regular,
-  Inter_600SemiBold,
-  Inter_700Bold,
-  Inter_900Black,
-  useFonts,
-} from '@expo-google-fonts/inter'
-
-import { Loading } from './src/components/Loading'
-import { Login } from './src/screens/Login'
-import { Register } from './src/screens/Register'
-import { Home } from './src/screens/Home'
-
-const Stack = createNativeStackNavigator()
+import Routes from './src/routes'
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    Inter_400Regular,
-    Inter_600SemiBold,
-    Inter_700Bold,
-    Inter_900Black,
-  })
-
   return (
     <>
-      <NavigationContainer>
-        {fontsLoaded ? (
-          <Stack.Navigator
-            initialRouteName="Login"
-            screenOptions={{
-              headerShown: false,
-            }}
-          >
-            <Stack.Screen name="Login" component={Login} />
-            <Stack.Screen name="Register" component={Register} />
-            <Stack.Screen name="Home" component={Home} />
-          </Stack.Navigator>
-        ) : (
-          <Loading />
-        )}
-      </NavigationContainer>
-
+      <Routes />
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
