66a1d352a2a980ba61a23ccae3d9c3dfe52ad912
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-11-02 16:32:59 -0300

    [*] added button logout

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,9 +2,10 @@ import { View, Text } from 'react-native'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { styles } from './styles'
 import { useState, useEffect } from 'react'
+import { ButtonComponent } from '../../components'
 
-export function Home() {
-  const [userData, setUserData] = useState<any>({})
+export function Home({ navigation }: any) {
+  const [userData, setUserData] = useState<any>([])
 
   useEffect(() => {
     AsyncStorage.getItem('userData', (err, result) => {
@@ -13,13 +14,21 @@ export function Home() {
     })
   }, [])
 
-  console.log(userData)
+  function logout() {
+    AsyncStorage.clear()
+    setUserData([])
+
+    setTimeout(() => {
+      navigation.push('Login')
+    }, 500)
+  }
 
   return (
     <View style={styles.container}>
-      {Object.keys(userData).length > 0 && (
+      {userData && (
         <Text style={styles.title}> Bem Vindo, {userData.name} ! </Text>
       )}
+      <ButtonComponent text="Logout" onPress={logout} />
     </View>
   )
 }
